927074cfffdc4905b076196dd808d28ff52d7e4b
8agus <merx570@gmail.com>
2022-11-03 18:29:56 +0400

    Added jump(), moved local_address outside value_from_cache() function

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-
+from random import randrange
 
 if len(sys.argv) != 3:
     print("Invalid number of arguments")
@@ -12,6 +12,10 @@ open_instr = open (sys.argv[1], "r")  # Fetch instructions from an input file
 # variable used to store values in  Memory Bus
 memo_reg = {}
 
+# Use ISA operand from instruction_input.txt as key to locate Source Register index position
+# Line 1 in data_input.txt is R1 at index 0 etc.
+# Index point is then used to retrieve or replace value
+loca_address = {"R1": 0, "R2": 1, "R3": 2, "R4": 3, "R5": 4, "R6": 5, "R7": 6}
 
 # Send initial Memory Bus values to the CPU and Memory Bus and clearing memory
 def cache(on_off):
@@ -34,16 +38,16 @@ def cache(on_off):
 counter = 0
 
 
-def write_to_register(data):
-    global counter
-    print("Memory before writing: ", memo_reg)
-    if len(memo_reg) > 7:
-        counter = 0
+def write_to_register(value_in, reg_loc_in):
+    print("*" * 60, 'Write to register', "*" * 60)
+    print("\nRegister before writing: ", memo_reg)
+    reg_address = value_from_cache([reg_loc_in])
+    print(f"Line 44 - Value {value_in} to be updated in register address {reg_address[1]}")
 
+    memo_reg[reg_address[1]] = value_in
 
-    print("Memory after writing: ", memo_reg)
-
-instruction_list = []
+    print("\nRegister after writing: ", memo_reg)
+    print("*" * 60, 'Register update successfully', "*" * 60)
 
 
 def read_instructions(opcode):
@@ -57,35 +61,52 @@ def read_instructions(opcode):
                 cache(final_instr[1])
             elif final_instr[0] == 'ADDI':
                 addi(final_instr)
+            elif final_instr[0] == 'ADD':
+                add(final_instr)
+            elif final_instr[0] == 'J':
+                jump(opcode)
 
 
 def addi(opcode):
     print('\nOpcode received in ADDI:', opcode)
     dest, location, value_2 = opcode[1], opcode[2], opcode[3]
     value_1 = value_from_cache([location])
-    answer = int(value_1) + int(value_2)
-    return print(f"\nThe answer for {value_1} + {value_2} is: {answer}")
+    answer = int(value_1[0][0]) + int(value_2)
+    write_to_register(answer, dest)
+    return print(f"\nThe answer for {value_1[0]} + {value_2} is: {answer}")
+
+
+def add(opcode):
+    print('\nOpcode received in ADD:', opcode)
+    dest, location_1, location_2 = opcode[1], opcode[2], opcode[3]
+    value, reg_add = value_from_cache([location_1, location_2])
+    print(f'Line 79 - Value is {value} and Reg Add is {reg_add}')
+    answer = int(value[0]) + int(value[1])
+    write_to_register(answer, dest)
+    return print(f"\nThe answer for {value[0]} + {value[1]} is: {answer}")
+
+
+# jump instruction used to create
+# simulation output
+def jump(opcode):
+    print(f"\nOpcode received in jump: {opcode}")
+    select_jump = randrange(0, 6)
+    value = list(memo_reg.values())[select_jump]
+    register = list(memo_reg.keys())[select_jump]
+    print(f"\nUsing Jump function register went to register address {register} with Value {value}")
 
 
 def value_from_cache(locations):
     values = []
-    loca_address = {
-        "R1": 0,
-        "R2": 1,
-        "R3": 2,
-        "R4": 3,
-        "R5": 4,
-        "R6": 5,
-        "R7": 6,
-    }
     for location in locations:
-        print("Line 82: ", location)
-        print("Line 83: ", loca_address[location])
+        print("Line 76: ", location)
+        print("Line 77: ", loca_address[location])
+
         register = list(memo_reg)[loca_address[location]]
-        values = list(memo_reg.values())[loca_address[location]]
+        values.append(list(memo_reg.values())[loca_address[location]])
         print(f"\nRetrieved value is {values} from location {register}.")
 
-    return values
+    return values, register
 
 
 read_instructions(open_instr)
