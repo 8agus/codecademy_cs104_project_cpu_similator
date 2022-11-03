f1ce5d26eebb1db0c7ede78776685207d42bc302
8agus <merx570@gmail.com>
2022-11-03 14:28:25 +0400

    Added read_instructions(), addi(), value_from_cache()

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import sys
 
 
-print(len(sys.argv))
-
 if len(sys.argv) != 3:
     print("Invalid number of arguments")
 
@@ -17,18 +15,20 @@ memo_reg = {}
 
 # Send initial Memory Bus values to the CPU and Memory Bus and clearing memory
 def cache(on_off):
-    if on_off:
+    if on_off == '1':
         print("\nMemory before loading: ", memo_reg)
         print("\nLoading Memory...")
         for line in open_data.readlines():
-            memo_reg[line[:9]] = line[9]
-            open_data.close()
+            memo_reg[line[:8]] = line[9]
         print("\nMemory loaded: ", memo_reg)
-    if not on_off:
+        print("\n")
+    else:
         print("\nMemory before halt: ", memo_reg)
         print("\nClearing memory...")
         memo_reg.clear()
-        print("\nMemory after halt: ", memo_reg)
+        print("\nMemory after HALT: ", memo_reg)
+        print("\n")
+        open_data.close()
 
 
 counter = 0
@@ -43,5 +43,49 @@ def write_to_register(data):
 
     print("Memory after writing: ", memo_reg)
 
+instruction_list = []
 
-# write_to_register(open_data)
+
+def read_instructions(opcode):
+    for line in opcode.readlines():
+        inst_list = line.split()
+        for item in inst_list:
+            final_instr = item.split(',')
+            print(final_instr, 'Line 52')
+            if final_instr[0] in ['CACHE', 'HALT']:
+                print('Line 56: ', final_instr[0])
+                cache(final_instr[1])
+            elif final_instr[0] == 'ADDI':
+                addi(final_instr)
+
+
+def addi(opcode):
+    print('\nOpcode received in ADDI:', opcode)
+    dest, location, value_2 = opcode[1], opcode[2], opcode[3]
+    value_1 = value_from_cache([location])
+    answer = int(value_1) + int(value_2)
+    return print(f"\nThe answer for {value_1} + {value_2} is: {answer}")
+
+
+def value_from_cache(locations):
+    values = []
+    loca_address = {
+        "R1": 0,
+        "R2": 1,
+        "R3": 2,
+        "R4": 3,
+        "R5": 4,
+        "R6": 5,
+        "R7": 6,
+    }
+    for location in locations:
+        print("Line 82: ", location)
+        print("Line 83: ", loca_address[location])
+        register = list(memo_reg)[loca_address[location]]
+        values = list(memo_reg.values())[loca_address[location]]
+        print(f"\nRetrieved value is {values} from location {register}.")
+
+    return values
+
+
+read_instructions(open_instr)
